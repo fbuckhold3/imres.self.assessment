5e1469484f9ed0b5cab2d3b01aca639d0a7a37a8
fbuckhold3 <78330963+fbuckhold3@users.noreply.github.com>
2025-04-30 11:09:13 -0500

    add github upload to app.R

--- app.R
+++ app.R
@@ -1,13 +1,23 @@
 library(shiny)
 
+# Try to install imres package from GitHub if it's not available
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+if (!requireNamespace("imres", quietly = TRUE)) {
+  message("Installing imres package from GitHub...")
+  remotes::install_github("fbuckhold3/imres")
+}
+
+
 source("R/global.R")  # Load all libraries, API calls, and data
 shinyApp(ui = source("R/ui.R")$value, server = source("R/server.R")$value)
 
 
-devtools::load_all("~/Documents/GitHub/IMSLU RDM/imres")
-shinyApp(ui, server)
+##devtools::load_all("~/Documents/GitHub/IMSLU RDM/imres")
+##shinyApp(ui, server)
 
-# WdyPEh 7uG6gS   NYqoWH   ujflcv  i9MmGj   TuAjIG
+
 #
 
 
